edd070ae9fd2ba07bb654ea401d4b759e7897522
Priyanshu Arora <priyarora4@gmail.com>
2020-12-14 21:21:30 -0500

    ensemble irt

--- part_b/ensembleB.py
+++ part_b/ensembleB.py
@@ -4,12 +4,14 @@ from utils import *
 from scipy.linalg import sqrtm
 
 import numpy as np
-from part_a.item_response import *
+from part_b.item_responseB import *
 
 from sklearn.impute import KNNImputer
 from utils import *
 import matplotlib.pyplot as plt
 from part_b.matrix_factorizationB import *
+
+g=0.25
 
 
 def bootstrap_data(data, size):
@@ -32,19 +34,19 @@ def bootstrap_data(data, size):
 
 def run_irt(train_data, val_data):
     best_lr = 0.01
-    best_iterations = 13
+    best_iterations = 17
 
     size = len(train_data['user_id'])
     train_data_bootstrap = bootstrap_data(train_data, size)
 
-    theta, beta, val_acc_lst, neg_lld_list_train, neg_lld_list_valid = irt(
+    theta, beta, a, val_acc_lst, neg_lld_list_train, neg_lld_list_valid = irt(
         train_data_bootstrap, val_data, best_lr, best_iterations)
 
     mat = np.zeros((len(theta), len(beta)))
 
     for i in range(len(theta)):
         for j in range(len(beta)):
-            mat[i][j] = np.exp(theta[i] - beta[j]) / (1 + np.exp(theta[i] - beta[j]))
+            mat[i][j] = (((1-g)*np.exp(a[j]*(theta[i] - beta[j]))) / (1 + np.exp(a[j]*theta[i] - beta[j]))) + g
 
     return mat
 
@@ -80,18 +82,22 @@ def main():
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
+    num_users = len(set(train_data['user_id']))
+    num_questions = len(set(train_data['question_id']))
 
-    # mat_irt = run_irt(train_data, val_data)
+
+
+
     # mat_knn = run_knn(train_data, val_data, k=11)
-    mat_fact1 = run_fact(train_data, val_data, k=388)
-    mat_fact2 = run_fact(train_data, val_data, k=388)
-    mat_fact3 = run_fact(train_data, val_data, k=388)
-    mat_fact4 = run_fact(train_data, val_data, k=388)
-    mat_fact5 = run_fact(train_data, val_data, k=388)
-    mat_fact6 = run_fact(train_data, val_data, k=388)
-    mat_fact7 = run_fact(train_data, val_data, k=388)
-    mat_fact8 = run_fact(train_data, val_data, k=388)
-    mat_fact9 = run_fact(train_data, val_data, k=388)
+    # mat_fact1 = run_fact(train_data, val_data, k=388)
+    # mat_fact2 = run_fact(train_data, val_data, k=388)
+    # mat_fact3 = run_fact(train_data, val_data, k=388)
+    # mat_fact4 = run_fact(train_data, val_data, k=388)
+    # mat_fact5 = run_fact(train_data, val_data, k=388)
+    # mat_fact6 = run_fact(train_data, val_data, k=388)
+    # mat_fact7 = run_fact(train_data, val_data, k=388)
+    # mat_fact8 = run_fact(train_data, val_data, k=388)
+    # mat_fact9 = run_fact(train_data, val_data, k=388)
     # irt_accuracy_train = sparse_matrix_evaluate(train_data, mat_irt)
     # fact2_accuracy_train = sparse_matrix_evaluate(train_data, mat_fact2)
     # fact_accuracy_train = sparse_matrix_evaluate(train_data, mat_fact)
@@ -111,10 +117,13 @@ def main():
     # print("Fact train accuracy {}".format(fact_accuracy_train))
     # print("Fact valid accuracy {}".format(fact_accuracy_valid))
     # print('\n')
+    num_mats = 5
+    ensemble = np.zeros((num_users, num_questions))
+    for i in range(num_mats):
+        mat = run_irt(train_data, val_data)
+        ensemble += mat
 
-
-    ensemble = (mat_fact1 + mat_fact2 + mat_fact3 + mat_fact4 + mat_fact5 + mat_fact6 + mat_fact7+ mat_fact8 + mat_fact9) / 9
-
+    ensemble = ensemble / num_mats
     train_accuracy = sparse_matrix_evaluate(train_data, ensemble)
     valid_accuracy = sparse_matrix_evaluate(val_data, ensemble)
 
